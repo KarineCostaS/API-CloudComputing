486e946db7e201ef37b86caf78e84904a2678f86
Karine Costa <karine.costadev@gmail.com>
2023-02-16 03:17:57 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,11 @@
+{ pkgs }: {
+	deps = [
+		pkgs.nodejs-16_x
+  pkgs.nodejs-16_x
+  pkgs.npm install uuid
+  pkgs.nodejs-18_x
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
+    pkgs.replitPackages.jest
+	];
+}
---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express"; //framework 
+import db from "./config/dbConect.js"; //bancodedados
+import routes from "./routes/index.js"; //rotas
+
+db.on("error", console.log.bind(console, 'erro de conexão'))
+db.once("open", () => {
+  console.log('conexão com o banco feita com sucesso') //verificar a conexão com o banco de dados
+})
+
+const app = express();
+
+app.use(express.json()) //colocar o express pra receber o json
+
+routes(app);
+
+
+export default app
